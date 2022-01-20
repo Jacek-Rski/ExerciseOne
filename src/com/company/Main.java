3b5c61ff073b7f2034e26ef965252f0e2707a716
Jacek <jacektheoboy@gmail.com>
2022-01-20 14:26:39 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+//Jacek Rzepczynski
+
+
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String city = "Franklin Township";
+        int zip = 43223;
+
+        double dailyHighs[] = {32, 25, 27, 40, 45};
+        double avgHigh = 32 + 25 + 27 + 40 + 45;
+        double avg = avgHigh / 5;
+
+        System.out.println("City: " + city +"Zip Code: " + zip + " Average High Temp" + avg);
+
+    }
+}
